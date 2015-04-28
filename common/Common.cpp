e2e13baa5b55df7eaa56f9240251b1c42dfb7089
sijianwudi <231603340@163.com>
2015-04-28 22:49:44 +0800

    update Common.cpp

--- common/Common.cpp
+++ common/Common.cpp
@@ -1,19 +1,10 @@
-//this file is part of notepad++
-//Copyright (C)2003 Don HO ( donho@altern.org )
-//
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
-//
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/**
+ * @file Common.cpp
+ * @brief some useful functions collected from other open source
+ * @author liangsijian
+ * @version 
+ * @date 2015-04-26
+ */
 #include <windows.h>
 #include <vector>
 #include <ShlObj.h>
@@ -21,6 +12,91 @@
 #include "Common.h"
 #pragma comment(lib,"shlwapi.lib")
 #pragma warning(suppress:4996)
+/* string {{{1 */
+std::vector<generic_string> tokenizeString(const generic_string & tokenString, const char delim) {
+	//Vector is created on stack and copied on return
+	std::vector<generic_string> tokens;
+
+    // Skip delimiters at beginning.
+	generic_string::size_type lastPos = tokenString.find_first_not_of(delim, 0);
+    // Find first "non-delimiter".
+    generic_string::size_type pos     = tokenString.find_first_of(delim, lastPos);
+
+    while (pos != std::string::npos || lastPos != std::string::npos)
+    {
+        // Found a token, add it to the vector.
+        tokens.push_back(tokenString.substr(lastPos, pos - lastPos));
+        // Skip delimiters.  Note the "not_of"
+        lastPos = tokenString.find_first_not_of(delim, pos);
+        // Find next "non-delimiter"
+        pos = tokenString.find_first_of(delim, lastPos);
+    }
+	return tokens;
+}
+
+/* search {{{2 */
+bool isInList(const TCHAR *token, const TCHAR *list) {
+	if ((!token) || (!list))
+		return false;
+	TCHAR word[64];
+	int i = 0;
+	int j = 0;
+	for (; i <= int(lstrlen(list)) ; i++)
+	{
+		if ((list[i] == ' ')||(list[i] == '\0'))
+		{
+			if (j != 0)
+			{
+				word[j] = '\0';
+				j = 0;
+				
+				if (!generic_stricmp(token, word))
+					return true;
+			}
+		}
+		else 
+		{
+			word[j] = list[i];
+			j++;
+		}
+	}
+	return false;
+}
+
+/* }}} search */
+/* other {{{2 */
+
+std::wstring string2wstring(const std::string & rString, UINT codepage)
+{
+	int len = MultiByteToWideChar(codepage, 0, rString.c_str(), -1, NULL, 0);
+	if(len > 0)
+	{		
+		std::vector<wchar_t> vw(len);
+		MultiByteToWideChar(codepage, 0, rString.c_str(), -1, &vw[0], len);
+		return &vw[0];
+	}
+	else
+		return L"";
+}
+
+std::string wstring2string(const std::wstring & rwString, UINT codepage)
+{
+	int len = WideCharToMultiByte(codepage, 0, rwString.c_str(), -1, NULL, 0, NULL, NULL);
+	if(len > 0)
+	{		
+		std::vector<char> vw(len);
+		WideCharToMultiByte(codepage, 0, rwString.c_str(), -1, &vw[0], len, NULL, NULL);
+		return &vw[0];
+	}
+	else
+		return "";
+}
+/* }}} other */
+/* }}} string */
+
+
+
+/* file {{{1 */
 void writeLog(const TCHAR *logFileName, const char *log2write)
 {	
 	FILE *f = generic_fopen(logFileName, TEXT("a+"));
@@ -29,6 +105,10 @@ void writeLog(const TCHAR *logFileName, const char *log2write)
 	fflush(f);
 	fclose(f);
 }
+
+/* }}} file */
+
+/* gui {{{1 */
 
 // Set a call back with the handle after init to set the path.
 // http://msdn.microsoft.com/library/default.asp?url=/library/en-us/shellcc/platform/shell/reference/callbackfunctions/browsecallbackproc.asp
@@ -108,255 +188,187 @@ void ClientRectToScreenRect(HWND hWnd, RECT* rect)
 	rect->bottom = pt.y;
 };
 
-std::vector<generic_string> tokenizeString(const generic_string & tokenString, const char delim) {
-	//Vector is created on stack and copied on return
-	std::vector<generic_string> tokens;
 
-    // Skip delimiters at beginning.
-	generic_string::size_type lastPos = tokenString.find_first_not_of(delim, 0);
-    // Find first "non-delimiter".
-    generic_string::size_type pos     = tokenString.find_first_of(delim, lastPos);
+/* }}} gui */
 
-    while (pos != std::string::npos || lastPos != std::string::npos)
+/* util {{{1 */
+/* directrion {{{2 */
+
+int IsValidDirectory(const TCHAR *path)
+{
+
+    int Rc;
+    Rc = GetFileAttributes(path);
+    return Rc == INVALID_FILE_ATTRIBUTES ? FALSE : Rc & FILE_ATTRIBUTE_DIRECTORY;
+
+} // IsValidDirectory
+
+
+////////////////////////
+// Creates a line of the dir.txt file
+// use a callback function as argument since ScanDir is used either to
+// create dir.txt or to dispaly the dir window
+
+void ScanDir(int(*f)(TCHAR *s, DWORD dw), DWORD dwParam, const TCHAR *szDirectory)
+{
+    WIN32_FIND_DATA  FindData;
+    FILETIME    FtLocal;
+    SYSTEMTIME  SysTime;
+    TCHAR        szLine[256], szFileSpec[_MAX_PATH + 5];
+    TCHAR        szDate[sizeof "jj/mm/aaaa"];
+    HANDLE      hFind;
+
+    szFileSpec[_MAX_PATH - 1] = 0;
+    lstrcpyn(szFileSpec, szDirectory, _MAX_PATH);
+    lstrcat(szFileSpec, TEXT("\\*.*"));
+    hFind = FindFirstFile(szFileSpec, &FindData);
+    if (hFind != INVALID_HANDLE_VALUE)
+    do
     {
-        // Found a token, add it to the vector.
-        tokens.push_back(tokenString.substr(lastPos, pos - lastPos));
-        // Skip delimiters.  Note the "not_of"
-        lastPos = tokenString.find_first_not_of(delim, pos);
-        // Find next "non-delimiter"
-        pos = tokenString.find_first_of(delim, lastPos);
+        // display only files, skip directories
+        if (FindData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)  continue;
+        FileTimeToLocalFileTime(&FindData.ftCreationTime, &FtLocal);
+        FileTimeToSystemTime(&FtLocal, &SysTime);
+        GetDateFormat(LOCALE_SYSTEM_DEFAULT,
+            DATE_SHORTDATE,
+            &SysTime,
+            NULL,
+            szDate, sizeof szDate);
+        szDate[sizeof "jj/mm/aaaa" - 1] = 0;    // truncate date
+        FindData.cFileName[62] = 0;      // truncate file name if needed
+        // dialog structure allow up to 64 char
+        if (FindData.nFileSizeLow < 0) {
+            continue;
+        }
+        wsprintf(szLine, TEXT("%s\t%s\t%u"),
+            FindData.cFileName, szDate, FindData.nFileSizeLow);
+
+        (*f) (szLine, dwParam);
+    } while (FindNextFile(hFind, &FindData));
+
+    FindClose(hFind);
+
+}  // ScanDir
+/* }}}  directrion */
+
+/* parse commandLine {{{2 */
+
+typedef std::vector<const TCHAR*> ParamVector;
+bool checkSingleFile(const TCHAR * commandLine) {
+    TCHAR fullpath[MAX_PATH];
+    ::GetFullPathName(commandLine, MAX_PATH, fullpath, NULL);
+    if (::PathFileExists(fullpath)) {
+        return true;
     }
-	return tokens;
+
+    return false;
 }
+void parseCommandLine(TCHAR * commandLine, ParamVector & paramVector) {
+    //params.erase(params.begin());	
+    //remove the first element, since thats the path the the executable (GetCommandLine does that)
+    //TCHAR stopChar = TEXT(' ');
+    //if (commandLine[0] == TEXT('\"')) {
+    //    stopChar = TEXT('\"');
+    //    commandLine++;
+    //}
+    ////while this is not really DBCS compliant, space and quote are in the lower 127 ASCII range
+    //while (commandLine[0] && commandLine[0] != stopChar)
+    //{
+    //    commandLine++;
+    //}
 
-void ScreenRectToClientRect(HWND hWnd, RECT* rect)
-{
-	POINT		pt;
+    //// For unknown reason, the following command :
+    //// c:\NppDir>notepad++
+    //// (without quote) will give string "notepad++\0notepad++\0"
+    //// To avoid the unexpected behaviour we check the end of string before increasing the pointer
+    //if (commandLine[0] != '\0')
+    //    commandLine++;	//advance past stopChar
 
-	pt.x		 = rect->left;
-	pt.y		 = rect->top;
-	::ScreenToClient( hWnd, &pt );
-	rect->left   = pt.x;
-	rect->top    = pt.y;
+    ////kill remaining spaces
+    //while (commandLine[0] == TEXT(' '))
+    //    commandLine++;
 
-	pt.x		 = rect->right;
-	pt.y		 = rect->bottom;
-	::ScreenToClient( hWnd, &pt );
-	rect->right  = pt.x;
-	rect->bottom = pt.y;
-};
-
-int filter(unsigned int code, struct _EXCEPTION_POINTERS *) 
-{
-    if (code == EXCEPTION_ACCESS_VIOLATION)
-        return EXCEPTION_EXECUTE_HANDLER;
-
-    return EXCEPTION_CONTINUE_SEARCH;
-}
-
-bool isInList(const TCHAR *token, const TCHAR *list) {
-	if ((!token) || (!list))
-		return false;
-	TCHAR word[64];
-	int i = 0;
-	int j = 0;
-	for (; i <= int(lstrlen(list)) ; i++)
-	{
-		if ((list[i] == ' ')||(list[i] == '\0'))
-		{
-			if (j != 0)
-			{
-				word[j] = '\0';
-				j = 0;
-				
-				if (!generic_stricmp(token, word))
-					return true;
-			}
-		}
-		else 
-		{
-			word[j] = list[i];
-			j++;
-		}
-	}
-	return false;
-}
-
-
-generic_string purgeMenuItemString(const TCHAR * menuItemStr, bool keepAmpersand)
-{
-	TCHAR cleanedName[64] = TEXT("");
-	size_t j = 0;
-	size_t menuNameLen = lstrlen(menuItemStr);
-	for(size_t k = 0 ; k < menuNameLen ; k++) 
-	{
-		if (menuItemStr[k] == '\t')
-		{
-			cleanedName[k] = 0;
-			break;
-		}
-		else if (menuItemStr[k] == '&')
-		{
-			if (keepAmpersand)
-				cleanedName[j++] = menuItemStr[k];
-			//else skip
-		}
-		else
-		{
-			cleanedName[j++] = menuItemStr[k];
-		}
-	}
-	cleanedName[j] = 0;
-	return cleanedName;
-};
-
-
-std::wstring string2wstring(const std::string & rString, UINT codepage)
-{
-	int len = MultiByteToWideChar(codepage, 0, rString.c_str(), -1, NULL, 0);
-	if(len > 0)
-	{		
-		std::vector<wchar_t> vw(len);
-		MultiByteToWideChar(codepage, 0, rString.c_str(), -1, &vw[0], len);
-		return &vw[0];
-	}
-	else
-		return L"";
-}
-
-std::string wstring2string(const std::wstring & rwString, UINT codepage)
-{
-	int len = WideCharToMultiByte(codepage, 0, rwString.c_str(), -1, NULL, 0, NULL, NULL);
-	if(len > 0)
-	{		
-		std::vector<char> vw(len);
-		WideCharToMultiByte(codepage, 0, rwString.c_str(), -1, &vw[0], len, NULL, NULL);
-		return &vw[0];
-	}
-	else
-		return "";
-}
-
-static TCHAR* convertFileName(TCHAR *buffer, const TCHAR *filename)
-{
-	TCHAR *b = buffer;
-	const TCHAR *p = filename;
-	while (*p)
-	{
-		if (*p == '&') *b++ = '&';
-		*b++ = *p++;
-	}
-	*b = 0;	
-	return buffer;
-}
-
-TCHAR *BuildMenuFileName(TCHAR *buffer, int len, int pos, const TCHAR *filename)
-{
-	TCHAR cwd[MAX_PATH];
-	buffer[0] = 0;
-	GetCurrentDirectory(_countof(cwd), cwd);
-	lstrcat(cwd, TEXT("\\"));
-
-	TCHAR *itr = buffer;
-	TCHAR *end = buffer + len - 1;
-	if (pos < 9)
-	{
-		*itr++ = '&';
-		*itr++ = '1' + (TCHAR)pos;
-	}
-	else if (pos == 9)
-	{
-		*itr++ = '1';
-		*itr++ = '&';
-		*itr++ = '0';
-	}
-	else
-	{
-		wsprintf(itr, TEXT("%d"), pos+1);
-		itr = itr + lstrlen(itr);
-	}
-	*itr++ = ':';
-	*itr++ = ' ';
-	if (0 == generic_strnicmp(filename, cwd, lstrlen(cwd)))
-	{
-		TCHAR cnvName[MAX_PATH];
-		const TCHAR *s1 = PathFindFileName(filename);
-		int len = lstrlen(s1);
-		if (len < (end-itr))
-		{
-			lstrcpy(cnvName, s1);
-		}
-		else
-		{
-			int n = (len-3-(itr-buffer))/2;
-			generic_strncpy(cnvName, s1, n);
-			lstrcpy(cnvName+n, TEXT("..."));
-			lstrcat(cnvName, s1 + lstrlen(s1) - n);
-		}
-		convertFileName(itr, cnvName);
-	}
-	else
-	{
-		TCHAR cnvName[MAX_PATH*2];
-		convertFileName(cnvName, filename);
-		PathCompactPathEx(itr, filename, len - (itr-buffer), 0);
-	}
-	return buffer;
-}
-
-generic_string PathRemoveFileSpec(generic_string & path)
-{
-    generic_string::size_type lastBackslash = path.find_last_of(TEXT('\\'));
-    if (lastBackslash == generic_string::npos)
-    {
-        if (path.size() >= 2 && path[1] == TEXT(':'))  // "C:foo.bar" becomes "C:"
-            path.erase(2);
-        else
-            path.erase();
+    //bool isFile = checkSingleFile(commandLine);	//if the commandline specifies only a file, open it as such
+    //if (isFile) {
+    //    paramVector.push_back(commandLine);
+    //    return;
+    //}
+    bool isInFile = false;
+    bool isInWhiteSpace = true;
+    paramVector.clear();
+    size_t commandLength = lstrlen(commandLine);
+    for (size_t i = 0; i < commandLength; i++) {
+        switch (commandLine[i]) {
+        case '\"': {										//quoted filename, ignore any following whitespace
+                       if (!isInFile) {	//" will always be treated as start or end of param, in case the user forgot to add an space
+                           paramVector.push_back(commandLine + i + 1);	//add next param(since zero terminated generic_string original, no overflow of +1)
+                       }
+                       isInFile = !isInFile;
+                       isInWhiteSpace = false;
+                       //because we dont want to leave in any quotes in the filename, remove them now (with zero terminator)
+                       commandLine[i] = 0;
+                       break; }
+        case '\t':	//also treat tab as whitespace
+        case ' ': {
+                      isInWhiteSpace = true;
+                      if (!isInFile)
+                          commandLine[i] = 0;		//zap spaces into zero terminators, unless its part of a filename
+                      break; }
+        default: {											//default TCHAR, if beginning of word, add it
+                     if (!isInFile && isInWhiteSpace) {
+                         paramVector.push_back(commandLine + i);	//add next param 
+                         isInWhiteSpace = false;
+                     }
+                     break; }
+        }
     }
-    else
-    {
-        if (lastBackslash == 2 && path[1] == TEXT(':') && path.size() >= 3)  // "C:\foo.exe" becomes "C:\"
-            path.erase(3);
-        else if (lastBackslash == 0 && path.size() > 1)  //   "\foo.exe" becomes "\"
-            path.erase(1);
-        else
-            path.erase(lastBackslash);
-    }
-	return path;
+    //the commandline generic_string is now a list of zero terminated strings concatenated, and the vector contains all the substrings
 }
 
-generic_string PathAppend(generic_string &strDest, const generic_string str2append)
+static int argc = 0;
+static LPSTR argv[32];
+
+static int SplitCmdLine(LPSTR lpszCmdLine)
 {
-	if (strDest == TEXT("") && str2append == TEXT("")) // "" + ""
-	{
-		strDest = TEXT("\\");
-		return strDest;
-	}
+    LPSTR p, q;
+    argv[0] = "{{application}}";
+    p = lpszCmdLine;
+    while (p != NULL &&  *p)
+    {  // for each argument 
+        while ((*p) && (*p == ' ')) p++; // skip over leading spaces 
+        if (*p == '"')
+        {
+            q = ++p; // skip double quotes
+            // scan to end of argument 
+            while ((*p) && (*p != '"'))  p++;
+            argv[++argc] = q;
+            if (*p)  *p++ = 0;
+        }
+        else if (*p)
+        { // delimited by spaces 
+            q = p;
+            while ((*p) && (*p != ' '))  p++;
+            argv[++argc] = q;
+            if (*p)  *p++ = 0;
+        }
+    }  // parse all "words"
+    // create empty 
+    argv[++argc] = NULL;
+    return argc;
+} // SplitCmdLine
+/* }}} parse commandLine */
 
-	if (strDest == TEXT("") && str2append != TEXT("")) // "" + titi
-	{
-		strDest = str2append;
-		return strDest;
-	}
+#include <time.h>
+#define MAXTIMESTR  20  //"mm/dd/yyyy/00:00:00"/0
+char* timetoa(time_t t)
+{
+    static char timebuf[MAXTIMESTR];
+    struct tm *tt = localtime(&t);
+    if (tt == NULL)  timebuf[0] = 0;
+    else           sprintf(timebuf, "%02d/%02d/%04d/%02d:%02d:%02d",
+        tt->tm_mon + 1, tt->tm_mday, tt->tm_year + 1900,
+        tt->tm_hour, tt->tm_min, tt->tm_sec);
+    return timebuf;
+} 
 
-	if (strDest[strDest.length() - 1] == '\\' && (str2append != TEXT("") && str2append[0] == '\\')) // toto\ + \titi
-	{
-		strDest.erase(strDest.length() - 1, 1);
-		strDest += str2append;
-		return strDest;
-	}
-
-	if ((strDest[strDest.length() - 1] == '\\' && (str2append != TEXT("") && str2append[0] != '\\')) // toto\ + titi
-		|| (strDest[strDest.length() - 1] != '\\' && (str2append != TEXT("") && str2append[0] == '\\'))) // toto + \titi
-	{
-		strDest += str2append;
-		return strDest;
-	}
-
-	// toto + titi
-	strDest += TEXT("\\");
-	strDest += str2append;
-
-	return strDest;
-}
+/* }}} util */
